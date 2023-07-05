a62ca638cd745404507fc75ef971351ed1a63501
charles-okunzo <okunzopeter01@gmail.com>
2023-07-05 18:22:04 +0300

    Todo app in react - submiting todo list items

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,15 +3,25 @@ import { useState } from "react"
 
 export default function Todo(){
     const [todo, setTodo] = useState("")
+    const [todoList, setTodoList] = useState([])
 
     const handleTodo = (event)=>{
         setTodo(event.target.value)
-        console.log(todo)
     }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault();
+        let tempList = todoList;
+        tempList.push(todo)
+        setTodoList(tempList)
+        console.log(tempList)
+        setTodo("")
+    }
+
     return(
         <div>
             <h2>Todo</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleTodo} type="text" value={todo} />
                 <button type="submit">Add</button>
             </form>
