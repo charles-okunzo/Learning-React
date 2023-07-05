ea89e84eab80421d73d2e44c0d0219641d1d2275
charles-okunzo <okunzopeter01@gmail.com>
2023-07-05 13:05:27 +0300

    COnditional rendering using ternary operator

--- src/components/ConditionalComponent.js
+++ src/components/ConditionalComponent.js
@@ -3,18 +3,24 @@ import Counter from "./Counter"
 import FunctionalCounter from "./FunctionalCounter"
 
 export default function ConditionalComponent(){
-    const [display, setDisplay] = useState(true)
-    let output; // initialise an element variable
-    if (display){
-        output = <h3>This is a Conditional component</h3>
-    } else {
-        output = <h3>There is nothing to see here!</h3>
-    }
+    const [display, setDisplay] = useState(false);
+    // let output; // initialise an element variable
+    // if (display){
+    //     output = <h3>This is a Conditional component</h3>
+    // } else {
+    //     output = <h3>There is nothing to see here!</h3>
+    // }
+    // return (
+    //     <div>
+    //         {output}
+    //     </div>
+    // )
 
-    return (
-        <div>
-            {output}
-        </div>
-    )
-    }
-    
+    return display ? 
+    <div>
+        <h3>This is a Conditional component</h3>
+    </div> : 
+    <div>
+        <h3>There is nothing to see here!</h3>
+    </div> ; // conditional rendering using ternary operater
+} 
