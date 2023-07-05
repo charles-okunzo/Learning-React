22e87db9d8401526db8398ddee1c0d70237cfb2d
charles-okunzo <okunzopeter01@gmail.com>
2023-07-05 13:50:55 +0300

    rendering list of objects

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,13 @@
 export default function Products(){
-    const products = ["Laptop", "Phone", "Modem", "Computer"];
-    const productList = products.map((product)=>(<h3>{product}</h3>)
+    // const products = ["Laptop", "Phone", "Modem", "Computer", "Laptop"];
+
+    const products = [
+        {id:1, name:"Laptop", price: 500},
+        {id:2, name:"Phone", price: 200},
+        {id:3, name:"Modem", price: 50},
+        {id:4, name:"Laptop", price: 950},
+    ]
+    const productList = products.map((product)=>(<h3 key = {product.id}>{product.name}</h3>)
 )
     return (
         <div>
