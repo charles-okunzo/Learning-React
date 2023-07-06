a6e8212dae57c07acca69033089516d30fb3f29a
charles-okunzo <okunzopeter01@gmail.com>
2023-07-06 21:04:08 +0300

    Styling using css modules

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Form from './components/Form';
 import Todo from './components/Todo';
 import InlineComponet from './components/InlineComponent';
 import StylesheetComponent from './components/StylesheetComponent';
+import ModuleComponent from './components/ModuleComponent';
 
 function App() {
   return (
@@ -32,7 +33,8 @@ function App() {
     {/* <Form /> */}
     {/* <Todo /> */}
     {/* <InlineComponet /> */}
-    <StylesheetComponent />
+    {/* <StylesheetComponent /> */}
+    <ModuleComponent />
 
     </div>
   );
---
+++ src/components/ModuleComponent.js
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css"
+
+export default function ModuleComponent(){
+    return(
+        <div className={styles.header}>
+            This is a Module Component
+        </div>
+    )
+}
